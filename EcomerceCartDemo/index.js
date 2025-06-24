d523360d276152e1f00c6579eaf15b902b99a164
bipna <bindushreeshpun@gmail.com>
2025-06-24 21:27:10 +0545

    add initial files

---
+++ EcomerceCartDemo/index.js
@@ -0,0 +1,5 @@
+const hamburger =document.getElementById('hamburger');
+const navlink = document.getElementById('navlink');
+hamburger.addEventListener('click', ()=>{
+    navlink.classList.toggle('show');
+})
